a5a5f2e088c0d1e54251f035d3fd5dbd51d31d66
Jiho <yujh9610@gmail.com>
2023-11-13 23:26:49 +0900

    메서드 선업 및 호출

---
+++ Hello.java
@@ -0,0 +1,12 @@
+public class Hello {
+    //메서드 선언
+    public static void showHello(){
+        System.out.println("***********************");
+        System.out.println(" * 안녕하세요. Hello * ");
+        System.out.println("***********************");
+    }
+    // 메서드 호출
+    public static void main(String[] args){
+        showHello(); // 메서드 이름()
+    }
+}
