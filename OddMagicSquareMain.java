59d0364f5a7239cc551de96902eb88dd8cf7754e
YJH <yujh9610@gmail.com>
2023-12-21 17:37:10 +0900

    홀수 마방진 만들기

---
+++ OddMagicSquareMain.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class OddMagicSquareMain {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        int n = scanner.nextInt();
+        System.out.println("홀수 정수를 입력하세요.");
+        OddMagicSquare odd = new OddMagicSquare(n);
+        odd.make();
+        odd.print();
+    }
+}
