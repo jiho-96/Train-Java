f2d57abf17b34d511dc111fc37bc33665c0c7473
jiho <145963790+jiho-96@users.noreply.github.com>
2023-12-22 23:40:24 +0900

    이중 for문으로 역삼각형 그리기

---
+++ StarPoint.java
@@ -0,0 +1,17 @@
+public class StarPoint{
+  public static void printStarDecrease(int n) {
+    for(int i=0; i<n; i++){
+      for(int j=0; j<i; j++){
+        System.out.print(" ");
+      }
+      for(int k=1; k<2*(n-i); k++) {
+        System.out.print("*");
+      }
+      System.out.println();
+  }
+}
+public static void main(String[] args) {
+  printStarDecrease(5);
+  }
+}
+    
