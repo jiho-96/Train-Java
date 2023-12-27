859179110f4f5fee1a5f53f4d3d86520ee44aad5
jiho-96 <yujh9610@gmail.com>
2023-12-27 23:59:04 +0900

    Calendar 이용하여 휴일 구하기

---
+++ RestDay.java
@@ -0,0 +1,30 @@
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+
+public class RestDay {
+    // 토, 일요일
+    public static boolean isRest(Calendar tod){
+        boolean isRest = false;
+        if (tod.get(Calendar.DAY_OF_WEEK)==Calendar.SATURDAY || //토요일
+        tod.get(Calendar.DAY_OF_WEEK)==Calendar.SUNDAY){ // 일요일
+            isRest = true;
+    }
+        return isRest;
+}
+// Calendar 문자열로
+public static String toYMD(Calendar dd){
+        SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd");
+        return sdf.format(dd.getTime());
+    }
+public static void main(String[] args){
+        Calendar cal = Calendar.getInstance();
+        int lastDay = cal.getActualMaximum(Calendar.DAY_OF_MONTH);
+        for (int i=1; i<=lastDay; i++){
+            cal.set(Calendar.DAY_OF_MONTH, i);// 일부터 마지막날 까지
+            if(isRest(cal)){
+                System.out.println(toYMD(cal)+"is Rest Day");
+            }
+        }
+    }
+}
+
