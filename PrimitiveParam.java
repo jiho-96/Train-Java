290d96944eee596f5c014031e717efc8d46936a6
jiho-96 <yujh9610@gmail.com>
2024-03-03 22:41:52 +0900

    기본형 매개변수

---
+++ PrimitiveParam.java
@@ -0,0 +1,17 @@
+class Data2 { int x; }
+public class PrimitiveParam {
+    public static void main(String[] args) {
+        Data2 d = new Data2();
+        d.x = 10;
+        System.out.println("main() : x = " + d.x);
+
+        change(d.x);
+        System.out.println("After change(d.x)");
+        System.out.println("main() : x = " + d.x);
+    }
+
+    static void change(int x){ // 기본형 매개변수
+        x = 1000;
+        System.out.println("change() : x = " + x);
+    }
+}
